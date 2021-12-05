0b8f67692b3da775d953de0577645bd6e1188b27
iamtaehoon <kimth9981@naver.com>
2021-12-05 15:13:23 +0900

    feat(LottoBall): 로또 공 생성

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -3,5 +3,9 @@ package lotto;
 public class Constant {
 	public static int LOTTO_TICKET_COST = 1000;
 	public static int LIMIT_TICKET_CNT = 30;
+	public static int MIN_LOTTO_BALL = 1;
+	public static int MAX_LOTTO_BALL = 45;
+
 	public static String NUMBER_REGEX = "-?\\d+";
+
 }
---
+++ src/main/java/lotto/LottoBall.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import static lotto.Constant.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoBall {
+	private static Map<Integer, LottoBall> lottoBallCache = new HashMap<>();
+
+	static {
+		for (int ballNum = MIN_LOTTO_BALL; ballNum < MAX_LOTTO_BALL; ballNum++) {
+			lottoBallCache.put(ballNum, new LottoBall(ballNum));
+		}
+	}
+
+	private int ballNum;
+
+	private LottoBall(int ballNum) {
+		this.ballNum = ballNum;
+	}
+
+	public LottoBall of(int ballNum) {
+		return lottoBallCache.get(ballNum);
+	}
+
+}

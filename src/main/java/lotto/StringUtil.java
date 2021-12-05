79521a5a86f70c616d9cd5b50e9742e163e4d7fd
iamtaehoon <kimth9981@naver.com>
2021-12-05 14:53:14 +0900

    feat(StringUtil): 수동 티켓에서 입력받은 숫자가 6개인지 검증

--- src/main/java/lotto/StringUtil.java
+++ src/main/java/lotto/StringUtil.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 
 public class StringUtil {
 	public static ArrayList<String> makeTicket(String input) {
-
+		ArrayList<String> eachNumbersInTicket = new ArrayList<>();
+		String[] numbers = input.split(", ");
+		if (numbers.length != 6) {
+			throw new IllegalArgumentException("번호는 여섯 개를 입력해야 합니다.");
+		}
+		for (String number : numbers) {
+			eachNumbersInTicket.add(number);
+		}
+		return eachNumbersInTicket;
 	}
 }
